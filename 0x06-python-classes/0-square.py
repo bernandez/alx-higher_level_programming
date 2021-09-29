cbf56def6f6435c544034473ade78a4f4d5d69b1
bernandez <alouzehm@gmail.com>
2021-09-29 23:58:28 +0100

    Updated classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines an empty square class"""
+
+
+class Square():
+    """an empty square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a square class"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """Initializing the square class"""
+        self.__size = size
